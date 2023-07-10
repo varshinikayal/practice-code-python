ac52a85b997bd089d2884f28f09a0da442a05cc9
varshinikayal <135025163+varshinikayal@users.noreply.github.com>
2023-07-10 18:27:58 +0530

    string_manipulation.py

---
+++ string_manipulation.py
@@ -0,0 +1,14 @@
+# Prints the string "Hello world" followed by a newline character and then prints "Hello world" again
+print("Hello world\n Hello world")
+
+# Concatenates the string "Hello" with the string "Varsh" and prints the result
+print("Hello"+"Varsh")
+
+# Concatenates the strings "Hello", a space character, and "varsh" and prints the result
+print("Hello"+ " " + "varsh")
+
+# Concatenates the string "Hello " with the string "Varsh" and prints the result
+print("Hello "+"Varsh")
+
+# Concatenates the string "Hello" with the string " varsh" and prints the result
+print("Hello"+" varsh")

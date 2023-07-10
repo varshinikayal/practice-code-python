82f72291204dc8212e15e8a69a466e67f4864c37
varshinikayal <135025163+varshinikayal@users.noreply.github.com>
2023-07-10 18:57:50 +0530

    typecasting.py

---
+++ typecasting.py
@@ -0,0 +1,44 @@
+# Integer to float
+x = 5
+y = float(x)
+print(x)
+
+# Float to integer
+a = 3.14
+b = int(a)
+print(b)
+
+# Integer to string
+num = 10
+str_num = str(num)
+print(str_num)
+
+# String to integer
+str_num = "20"
+int_num = int(str_num)
+print(int_num)
+
+# Float to string
+pi = 3.14
+str_pi = str(pi)
+
+# String to float
+str_float = "3.14"
+float_num = float(str_float)
+
+# String to boolean
+str_bool = "True"
+bool_value = bool(str_bool)
+
+# Boolean to integer
+bool_value = True
+int_bool = int(bool_value)
+
+length=3
+print(type(length))
+
+#Add and print the two  values
+print(int("10") +int ("10"))
+
+
+

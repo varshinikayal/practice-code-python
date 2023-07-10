e94a4672a378c5f9675e1d26509e9dc9db993a04
varshinikayal <135025163+varshinikayal@users.noreply.github.com>
2023-07-10 18:35:58 +0530

    input_function.py

---
+++ input_function.py
@@ -0,0 +1,9 @@
+# the user to enter their name and stores it in the variable "name"
+name = input("What is your name? ")
+
+# the user to enter how they are feeling and stores it in the variable "response"
+response = input("How are you? ")
+
+# Prints a greeting message by concatenating the strings "Hello ", the value of the variable "name",
+# ", ", and the value of the variable "response"
+print("Hello " + name + ", " + response)

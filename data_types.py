89ca7d09d818f6598e29fbdab206d27f63f080a8
varshinikayal <135025163+varshinikayal@users.noreply.github.com>
2023-07-10 18:44:08 +0530

    data_types.py

---
+++ data_types.py
@@ -0,0 +1,29 @@
+# Integer
+age = 18
+
+# Float
+height = 1.25
+
+# String
+name = "Varshu"
+
+# Boolean
+is_student = True
+
+# List
+fruits = ["apple", "grapes", "orange"]
+
+# Tuple
+coordinates = (10, 20)
+
+# Dictionary
+person = {"name": "kayal", "age": 26, "city": "Palani"}
+
+# Print the values along with their data types
+print("Age:", age, type(age))
+print("Height:", height, type(height))
+print("Name:", name, type(name))
+print("Is Student:", is_student, type(is_student))
+print("Fruits:", fruits, type(fruits))
+print("Coordinates:", coordinates, type(coordinates))
+print("Person:", person, type(person))
